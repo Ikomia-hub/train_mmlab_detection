04bb1c7758b9536c59193559112aabeb9ff73bd8
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 10:22:25 +0200

    Add test file

---
+++ train_mmlab_detection_test.py
@@ -0,0 +1,16 @@
+import logging
+from ikomia.utils.tests import run_for_test
+
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::train mmlab detection =====")
+    input_dataset = t.get_input(0)
+    params = t.get_parameters()
+    params["epochs"] = "2"
+    params["batch_size"] = "1"
+    params["dataset_split_ratio"] = "0.5"
+    t.set_parameters(params)
+    input_dataset.load(data_dict["datasets"]["detection"]["dataset_coco"])
+    yield run_for_test(t)
